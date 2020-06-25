92838533902ed28ffde9e2eb2be551c393a590d8
Eric Hicks <eric.hicks.8989@gmail.com>
2020-06-25 14:18:59 -0400

    featL Created functionality to allow airlines to be registered and funded
    - Created client code to interact with smart contract allowing airlines to be register or funded.
    - Updated modules to allow for `webpack-dev-server` to be run for client.

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -7,20 +7,21 @@ export default class Contract {
 
         let config = Config[network];
         this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
-        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress, config.dataAddress);
         this.initialize(callback);
         this.owner = null;
         this.airlines = [];
+        this.flights = [];
         this.passengers = [];
     }
 
     initialize(callback) {
         this.web3.eth.getAccounts((error, accts) => {
-           
+
             this.owner = accts[0];
 
             let counter = 1;
-            
+
             while(this.airlines.length < 5) {
                 this.airlines.push(accts[counter++]);
             }
@@ -31,6 +32,21 @@ export default class Contract {
 
             callback();
         });
+    }
+
+    async registerAirline(airline, name, callback) {
+        let self = this;
+        await self.flightSuretyApp.methods.registerAirline(airline, name).send({
+            from: self.owner
+        }, callback);
+    }
+
+    async payAirline(airline, value, callback) {
+        let self = this;
+        await self.flightSuretyApp.methods.payAirline(airline).send({
+            from: self.owner,
+            value: self.web3.utils.toWei(value, "ether")
+        }, callback);
     }
 
     isOperational(callback) {
@@ -46,7 +62,7 @@ export default class Contract {
             airline: self.airlines[0],
             flight: flight,
             timestamp: Math.floor(Date.now() / 1000)
-        } 
+        }
         self.flightSuretyApp.methods
             .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
             .send({ from: self.owner}, (error, result) => {
