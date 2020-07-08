d6dc7f830af9450e8b18cee7ccd347b1772f09f4
Eric Hicks <eric.hicks.8989@gmail.com>
2020-07-07 20:08:18 -0400

    Deployed
    - Deployed project
    
    Closes #28

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,8 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require('fs');
+
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = "c1d022ea24e94d51b7ae01f1f4df5e97";
 
 module.exports = {
   networks: {
@@ -13,6 +16,12 @@ module.exports = {
     develop: {
       accounts: 25,
       defaultEtherBalance: 500,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 8500000,
+      gasPrice: 20000000000
     }
   },
   compilers: {
